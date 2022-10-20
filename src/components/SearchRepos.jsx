2a254ad4e130ac7c49c9b2b58e206fc515c11cda
EricFooSD <fooshida@gmail.com>
2022-10-20 13:01:07 +0800

    create placeholder component for searching repos

---
+++ src/components/SearchRepos.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Box } from '@mui/material';
+// ===================================================>
+
+export default function SearchRepos() {
+  return (
+    <Box sx={{ my: 3, ml: 1 }}>
+      <h3>This feature is a work in progress</h3>
+    </Box>
+  );
+}
